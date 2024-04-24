2654c4b356f8bde6800f01a8ef2d0d10f351cb83
Odysseas Bournas <odysseas.bournas@nokia.com>
2024-04-24 12:44:55 +0530

    fix: nvim webdevicons

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,9 +5,9 @@ return {
     "nvim-tree/nvim-web-devicons",
     dependencies = { "DaikyXendo/nvim-material-icon" },
     config = function()
-      require("nvim-web-devicons").setup {
-        override = require("nvim-material-icon").get_icons(),
-      }
+      -- require("nvim-web-devicons").setup {
+      --   override = require("nvim-material-icon").get_icons(),
+      -- }
     end,
   },
   { "yamatsum/nvim-nonicons", config = true, enabled = false },
