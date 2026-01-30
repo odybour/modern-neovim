946ba398f9737af6787cb24c89bbc81d3b2e2533
Odysseas Bournas <odysseas.bournas@nokia.com>
2026-01-30 13:59:09 +0200

    .

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ if vim.g.vscode then
     vim.fn.VSCodeNotify "editor.action.goToReferences"
   end)
   -- https://github.com/vscode-neovim/vscode-neovim/issues/298
-  vim.opt.clipboard:append("unnamedplus")
+  vim.opt.clipboard:append "unnamedplus"
 else
   require "config.options"
   require "config.lazy"
